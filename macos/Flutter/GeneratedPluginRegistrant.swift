576f1b98aa5441ff529156eb5265b999e6afb697
yossefebrahim <yossef.ebrahim.me@gmail.com>
2026-02-08 02:37:55 +0200

    auto-claude: subtask-7-3 - Add audio recording route to AppRouter
    Added import for AudioRecordingScreen and new route at '/audio-recording'
    that follows the existing pattern, passing optional noteId via state.extra.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import isar_flutter_libs
+import just_audio
 import path_provider_foundation
+import record_darwin
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
